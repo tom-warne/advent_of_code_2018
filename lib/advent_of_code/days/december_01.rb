4995f60a7b7634949befb18752469504b4c2e2c8
Tom Warne <thomaswarne+github@gmail.com>
2018-12-03 07:03:13 -0800

    Continue working on finalizing support system; Update YARD documentation

--- lib/advent_of_code/days/december_01.rb
+++ lib/advent_of_code/days/december_01.rb
@@ -9,7 +9,7 @@ module AdventOfCode
 
       # Solves the December 1st Silver Puzzle
       #
-      # @return [Int<484>] the sum of all given frequency changes
+      # @return [Integer<484>] the sum of all given frequency changes
       def self.sum_frequency_changes!
         FREQUENCY_CHANGES.sum(INITIAL_FREQUENCY)
       end
@@ -22,10 +22,11 @@ module AdventOfCode
       }
 
       # Solves the December 1st Gold Puzzle
-      # @param  [Hash<Int, Nil>] freqs the table of frequencies that `freq` is being appended to
-      # @param  [Int]            freq  the frequency to start the table on
       #
-      # @return [Int<367>] the first frequency that is already contained in `freqs`
+      # @param  [Hash<Integer, Nil>] freqs the table of frequencies that `freq` is being appended to
+      # @param  [Integer]            freq  the frequency to start the table on
+      #
+      # @return [Integer<367>] the first frequency that is already contained in `freqs`
       def self.find_repeated_frequency!(freqs: {INITIAL_FREQUENCY => nil}, freq: INITIAL_FREQUENCY)
         FREQUENCY_CHANGES.each do |change|
           freq += change
