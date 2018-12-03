c24dbe41559e7fa65a6116456bbe211ed15236e6
Tom Warne <thomaswarne+github@gmail.com>
2018-12-02 21:35:44 -0800

    Create `advent_of_code.rb` to control the running of the tests.  Partition out each day with its own inputs and tests. Create CompletionMessage for formatting and controlling console output of tests. Create `december_01` as the first exmple of the daily advent tests

--- advent_of_code.rb
+++ advent_of_code.rb
@@ -1,12 +1,27 @@
-# Each day as the main file is built it will automatically be incorporated
-#  into the requirements.  -TW
+# frozen_string_literal: true
+
+require 'date'
+
+require_relative 'completion_message'
+
+# Each day when the main file is created it will automatically
+# be incorporated into the requirements.  -TW
+#
 Dir.glob('**/').each { |dir| require_relative "#{dir}#{dir.chomp('/')}" }
 
 class AdventOfCode
-  def initialize
-    print_calendar!
+  START_DATE = Date.parse('2018-12-01').freeze
+  END_DATE   = Date.parse('2018-12-01').freeze
+  DATE_RANGE = (START_DATE..END_DATE).freeze
+
+  TYPES = %i[GOLD SILVER].freeze
+
+  def self.solve!
+    DATE_RANGE.each do |date|
+      TYPES.each do |type|
+        CompletionMessage.new(const_get "AdventOfCode::#{date.strftime('%B%d')}::#{TYPE}_PUZZLE")
+      end
+    end
   end
 
-  def print_calendar
-  end
 end
